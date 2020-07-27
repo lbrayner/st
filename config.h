e95e75d843e235861ed2a5a7ea00cbf5304720f9
lbrayner <lbrayner@users.noreply.github.com>
2020-07-26 23:54:26 -0300

    Applied patch: font2

--- config.h
+++ config.h
@@ -6,6 +6,11 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Bitstream Vera Sans Mono:size=11:antialias=true:autohint=true";
+static char *font2[] = {
+	"Inconsolata for Powerline:size=11:antialias=true:autohint=true",
+/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+};
+
 static int borderpx = 2;
 
 /*
