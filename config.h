5657c56bbae530b8a04250ea9fd04c436d8aa7ae
lbrayner <lbrayner@users.noreply.github.com>
2020-07-26 23:24:47 -0300

    C-F2 swaps background colors

--- config.h
+++ config.h
@@ -213,7 +213,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ XK_ANY_MOD,           XK_F6,          swapcolors,     {.i =  0} },
+	{ ControlMask,          XK_F2,          swapcolors,     {.i =  0} },
 };
 
 /*
